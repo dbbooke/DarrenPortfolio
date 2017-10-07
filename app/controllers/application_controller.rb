ead7dafb1111a6cf9be861be340b7f1ffe9ce1c5
Darren Booker <darrenbooker@Darrens-MacBook-Pro.local>
2017-10-06 18:18:58 -0700

    Installed petergate gem for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+  	@copyright = DarrenViewTool::Renderer.copyright 'Darren Booker', 'All rights reserved'
+  end
+end
+
+
+module DarrenViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
 end
